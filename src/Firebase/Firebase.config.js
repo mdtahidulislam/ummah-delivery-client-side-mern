4e47abda89086834a0568ab2efab6e92d307aa8b
tahidul1 <contact.tahidul@gmail.com>
2021-11-04 00:54:46 +0600

    changed firebase config

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCbpZ_ddgSi0g8tk9sWDLk3rhaTUaKGSwA",
-    authDomain: "ummah-food-delivery.firebaseapp.com",
-    projectId: "ummah-food-delivery",
-    storageBucket: "ummah-food-delivery.appspot.com",
-    messagingSenderId: "383176384555",
-    appId: "1:383176384555:web:93d270ff81b7c6a2114a6e"
+    apiKey: "AIzaSyDYvdMkXYj8nZS1bFTgqTklZc0ky5bXMHs",
+    authDomain: "ummah-delivery-network.firebaseapp.com",
+    projectId: "ummah-delivery-network",
+    storageBucket: "ummah-delivery-network.appspot.com",
+    messagingSenderId: "1006988637357",
+    appId: "1:1006988637357:web:087e48ed83c5df2692c224"
 };
 
 export default firebaseConfig;
